f76eedce1603f70286e98f7f1d1256618fd5a345
Chanakkya <117065037+Chanakkya@users.noreply.github.com>
2023-12-11 10:39:35 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 10:33:44 2023
+
+@author: CHANAKIYA
+"""
+
+import streamlit as st
+
+st.title('Find the Largest Number')
+
+number1 = st.number_input('Enter the first number')
+number2 = st.number_input('Enter the second number')
+number3 = st.number_input('Enter the third number')
+
+if st.button('Find Largest'):
+    largest = max(number1, number2, number3)
+    st.success(f'The largest number is: {largest}')
