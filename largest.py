e5d8c2da8adf3d18a159dc5e5bf7388e8b9b890f
Chanakkya <117065037+Chanakkya@users.noreply.github.com>
2023-12-11 11:19:05 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -7,7 +7,7 @@ Created on Mon Dec 11 10:33:44 2023
 
 import streamlit as st
 
-st.title('Largest Of Three Number')
+st.title('Largest Of Three Number\n By Chanakkya S - 21F3003211')
 
 number1 = st.number_input('Enter the first number')
 number2 = st.number_input('Enter the second number')
